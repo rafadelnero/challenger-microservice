61ba04bf39a90d06e6aad30e40ebd7738a633e35
rafadelnero <rafacdelnero@gmail.com>
2020-09-12 08:52:58 +0100

    - Testing

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,8 +3,8 @@
 # Copy the new jar to the build location
 cp -f challenger-microservice/target/*.jar jenkins/build/
 
-echo "****************************"
-echo "** Building Docker Image ***"
-echo "****************************"
+echo "***************************"
+echo "** Building Docker Image **"
+echo "***************************"
 
 cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
