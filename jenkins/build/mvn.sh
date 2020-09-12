e9f70b1e8cd822387dbd6830ed64fce7384c78f3
rafadelnero <rafacdelnero@gmail.com>
2020-09-12 08:37:03 +0100

    - Testing

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 WORKSPACE=/var/jenkins_home/workspace/eline_deploy-application-jenkins
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
