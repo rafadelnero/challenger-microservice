14a8a3ff94e81960cc65b81af0cecbae8e321420
rafadelnero <rafacdelnero@gmail.com>
2020-09-13 08:04:29 +0100

    - Testing

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "***************************"
+echo "****************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/var/jenkins_home/workspace/eline_deploy-application-jenkins
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-challenger-microservice
 
-docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/challenger-microservice:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
