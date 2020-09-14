713dcc7710100766643ec480d1a2a60b008b03eb
rafadelnero <rafacdelnero@gmail.com>
2020-09-14 08:54:37 +0100

    - Testing

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -9,4 +9,4 @@ echo "ls $PWD"
 
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-challenger-microservice
 
-docker run --rm  -v  $WORKSPACE/challenger-microservice:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  ./*:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
