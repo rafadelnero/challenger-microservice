e56ccefd167e689e822eb542739701d59d00dcd2
rafadelnero <rafacdelnero@gmail.com>
2020-09-14 09:02:41 +0100

    - Testing

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -9,4 +9,4 @@ echo "ls $PWD"
 
 WORKSPACE=/var/jenkins_home/workspace/eline_deploy-application-jenkins
 
-docker run --rm  -v  $WORKSPACE/:/app/ -v /root/.m2/:/root/.m2/ -w /app/ maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE:/app/ -v /root/.m2/:/root/.m2/ -w /app/ maven:3-alpine "$@"
