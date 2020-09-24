40ec66f51c1530624c8b1532a83c4c04c15b63c7
rafadelnero <rafacdelnero@gmail.com>
2020-09-24 08:58:15 +0100

    - Testing

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -7,6 +7,6 @@ echo "****************************"
 echo "TEst ls" ls
 echo "ls $PWD"
 
-WORKSPACE=/var/jenkins_home/workspace/eline_deploy-application-jenkins
+WORKSPACE=/var/jenkins_home/workspace/challenger-microservice-pipeline
 
-docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSPACE/challenger-microservice:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
