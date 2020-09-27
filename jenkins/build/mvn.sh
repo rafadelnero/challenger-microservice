5a656bde5dfe4d98aa57be2357b9d25383e938d4
Juan M.vi <earth001@gmail.com>
2020-09-27 13:23:06 -0300

    Added sample mapping with maven image

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -7,3 +7,5 @@ echo "****************************"
 WORKSPACE=/var/jenkins_home/workspace/rvice_deploy-application-jenkins
 
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+
+# docker run --rm -it -v /root/.m2/:/root/.m2/ -v /var/jenkins_home/workspace/rvice_deploy-application-jenkins/:/app/ -w /app maven:3-alpine bash
