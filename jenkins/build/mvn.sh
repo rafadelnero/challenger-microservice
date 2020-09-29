c5d4c06387fbb5e070125152f5e07f527253134f
Juan M.vi <earth001@gmail.com>
2020-09-28 23:00:22 -0300

    Added new workspace mapping

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,8 +4,13 @@ echo "****************************"
 echo "** Building jar *************"
 echo "****************************"
 
-WORKSPACE=/var/jenkins_home/workspace/rvice_deploy-application-jenkins
+# WORKSPACE=/var/jenkins_home/workspace/rvice_deploy-application-jenkins
+
+# TODO Fix mapping, the current setup use the docker volume folder
+WORKSPACE=/var/lib/docker/volumes/ubuntu_jenkins-home/_data/workspace/rvice_deploy-application-jenkins
 
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
 # docker run --rm -it -v /root/.m2/:/root/.m2/ -v /var/jenkins_home/workspace/rvice_deploy-application-jenkins/:/app/ -w /app maven:3-alpine bash
+
+# docker run --rm -it -v /root/.m2/:/root/.m2/ -v /var/lib/docker/volumes/ubuntu_jenkins-home/_data/workspace/rvice_deploy-application-jenkins:/app/ -w /app maven:3-alpine bash
