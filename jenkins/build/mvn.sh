bab4bf6e6918016205110980cfda5172cd3390ea
Juan M.vi <earth001@gmail.com>
2020-09-28 23:08:12 -0300

    Update maven tag to support jdk 11

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -9,7 +9,7 @@ echo "****************************"
 # TODO Fix mapping, the current setup use the docker volume folder
 WORKSPACE=/var/lib/docker/volumes/ubuntu_jenkins-home/_data/workspace/rvice_deploy-application-jenkins
 
-docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11-slim "$@"
 
 # docker run --rm -it -v /root/.m2/:/root/.m2/ -v /var/jenkins_home/workspace/rvice_deploy-application-jenkins/:/app/ -w /app maven:3-alpine bash
 
