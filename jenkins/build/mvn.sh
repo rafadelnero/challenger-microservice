aa18b893d10c13357502a85c523d243fea2e0d19
rafadelnero <rafacdelnero@gmail.com>
2020-10-03 09:03:10 +0100

    - Testing

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,10 +8,12 @@ echo "****************************"
 
 # TODO Fix mapping, the current setup use the docker volume folder
 echo "Current workspace: "$WORKSPACE
-WORKSPACE=$(pwd)
-echo $WORKSPACE
+WORKSPACE=/var/lib/docker/volumes/ubuntu_jenkins-home/_data/workspace/rvice_deploy-application-jenkins
 
-docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11-slim "$@"
+ABSOLUTE_WORKSPACE="/var/lib/docker/volumes/ubuntu_jenkins-home/_data/workspace"
+JOB_WORKSPACE="\${PWD##*/}"
+
+docker run --rm  -v ${ABSOLUTE_WORKSPACE}/${JOB_WORKSPACE}:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11-slim "$@"
 
 # docker run --rm -it -v /root/.m2/:/root/.m2/ -v /var/jenkins_home/workspace/rvice_deploy-application-jenkins/:/app/ -w /app maven:3-alpine bash
 
